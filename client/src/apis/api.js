8d773226d661f2a6d4f3ea5498218918d76ee606
CRM Crew <crmcrew2021@gmail.com>
2021-02-24 14:19:57 +0200

    design changes

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -5,7 +5,7 @@ const cookie = new Cookies();
 const token = cookie.get('token');
 //  'https://crm-server-001.herokuapp.com'
 export default axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://crm-server-001.herokuapp.com',
   headers: {
     'Authorization': `Bearer ${cookie.get('token')}`,
   },
