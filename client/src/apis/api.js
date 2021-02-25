b519662dd4517c46272ddf281779ec55654f4ed9
CRM Crew <crmcrew2021@gmail.com>
2021-02-25 02:04:56 +0200

    :apple: added variable to check baseURL

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -3,11 +3,19 @@ import Cookies from 'universal-cookie';
 const cookie = new Cookies();
 
 const token = cookie.get('token');
-//https://crm-server-001.herokuapp.com/
+console.log('token', token);
+let baseURL = 'https://crm-server-001.herokuapp.com/';
+baseURL = 'http://localhost:5000';
+console.log(' test', process.env.REACT_APP_BACKEND_URL);
+baseURL =
+  process.env.REACT_APP_BACKEND_URL || 'https://crm-server-001.herokuapp.com';
+
+let headers = {};
+
+headers.Authorization = `Bearer ${cookie.get('token')}`;
+
 export default axios.create({
-  baseURL: 'https://crm-server-001.herokuapp.com',
-  headers: {
-    'Authorization': `Bearer ${cookie.get('token')}`,
-  },
+  baseURL: baseURL,
+  headers,
   withCredentials: true,
 });
