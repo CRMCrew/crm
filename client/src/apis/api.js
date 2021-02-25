f7a4475893e3e91e094c2022df51fabc69b1a3a0
CRM Crew <crmcrew2021@gmail.com>
2021-02-25 02:28:02 +0200

    trying to use react process on development

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -4,11 +4,8 @@ const cookie = new Cookies();
 
 const token = cookie.get('token');
 console.log('token', token);
-let baseURL = 'https://crm-server-001.herokuapp.com/';
-baseURL = 'http://localhost:5000';
-console.log(' test', process.env.REACT_APP_BACKEND_URL);
-baseURL =
-  process.env.REACT_APP_BACKEND_URL || 'https://crm-server-001.herokuapp.com';
+
+const baseURL = process.env.REACT_APP_BACKEND_URL;
 
 let headers = {};
 
