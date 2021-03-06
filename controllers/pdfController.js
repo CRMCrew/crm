ef9084a696a3ac54da08df84f99a3a217a444b20
CRM Crew <crmcrew2021@gmail.com>
2021-03-06 19:39:14 +0200

    test 2

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -4,21 +4,20 @@ var appDir = path.dirname(require.main.filename);
 const template = require('../pdf/index');
 
 const save = async (req, res) => {
-  pdf
-    .create(template(req.body, {}))
-    .toFile(`./pdf/${req.body.customerId}/${req.body.itemId}.pdf`, (err) => {
-      if (err) {
-        res.status(400).send({ error: err });
-      } else {
-        res.status(200).send({ success: 0 });
-      }
-    });
+  pdf.create(template(req.body, {})).toFile(`${req.body.itemId}.pdf`, (err) => {
+    if (err) {
+      res.status(400).send({ error: err });
+    } else {
+      res.status(200).send({ success: 0 });
+    }
+  });
 };
 
 const get = async (req, res) => {
   const { customerId, itemId } = req.params;
-  console.log(customerId);
-  const dir = `./pdf/${customerId}/${itemId}.pdf`;
+
+  const dir = `${appDir}/${itemId}.pdf`;
+  console.log(' dir', dir);
   res.sendFile(dir);
 };
 
