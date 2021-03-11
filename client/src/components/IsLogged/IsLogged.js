ea67703450f89d4343394168f5aa4ed027382c74
MotiLM2003 <motiphone2003@gmail.com>
2021-03-11 03:05:33 +0200

    asdsa

--- client/src/components/IsLogged/IsLogged.js
+++ client/src/components/IsLogged/IsLogged.js
@@ -4,27 +4,29 @@ import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { login } from '../../actions/customersActions';
 const IsLogged = (props) => {
-  console.log('p', props.customer);
   const history = useHistory();
   const [user, setUser] = useState(null);
   useEffect(() => {
     const getUser = async () => {
       const { data } = await api.get('/refresh/');
       setUser(data);
+      console.log(data);
       const customerDetails = {
         email: data.email,
         password: data.userPassword,
       };
-
-      await props.login(customerDetails, false);
+      const user = await props.login(customerDetails, false);
+      console.log(user);
+      setTimeout(() => {
+        history.push('/home');
+      }, 1000);
       //eslint-disable-next-line
-      history.push('/home');
     };
 
     getUser();
   }, []);
 
-  return user && <div>hello</div>;
+  return user && <div className='is-logged'> </div>;
 };
 
 const stateToProps = (state) => {
