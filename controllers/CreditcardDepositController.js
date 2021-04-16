c0241fa4088b7265129ec4de41dd3c373f99c2bf
CRM Crew <crmcrew2021@gmail.com>
2021-04-16 11:16:04 +0300

    asdasd

--- controllers/CreditcardDepositController.js
+++ controllers/CreditcardDepositController.js
@@ -1,9 +1,12 @@
 const CreditCardHistory = require('../models/CreditcardDeposit');
+const Test = require('../models/Test');
 
 const getResponse = async (req, res) => {
   const cus = new CreditCardHistory(req.body);
+  const test = new Test({ outPut: JSON.stringify(req.body) });
   try {
     const creditHistory = await cus.save();
+    const damn = test.save(test);
     res.status(201).send(creditHistory);
   } catch (err) {
     console.log(err);
